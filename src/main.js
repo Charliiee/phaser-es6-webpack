1c7da582875ff6415be784fb7c7a49b2ad3e1fdf
Charliiee <carlin.afonso@gmail.com>
2018-02-28 22:24:16 -0300

    Resize game manually on window resize

--- src/main.js
+++ src/main.js
@@ -16,4 +16,25 @@ class Game extends Phaser.Game {
   }
 }
 
-window.game = new Game();
+window.onload = function() {
+  window.game = new Game();
+  resize();
+  window.addEventListener("resize", resize, false);
+};
+
+function resize() {
+  console.log(window.game)
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+  if(windowRatio < gameRatio){
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+  }
+  else{
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
