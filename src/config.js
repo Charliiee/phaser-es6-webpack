548712d3010aefced366911580d90c893de50b20
Charliiee <carlin.afonso@gmail.com>
2018-02-28 22:23:39 -0300

    Add more options for tweaking game config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,25 @@
 import Phaser from 'phaser';
 
 export default {
+  title: 'Game Title',
   type: Phaser.AUTO,
   parent: 'content',
   width: 800,
   height: 600,
+  fps: {
+    min: 30,
+    target: 60
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  pixelArt: false,
+  autoResize: true,
+  roundPixels: false,
+  transparent: false,
   localStorageName: 'phaseres6webpack'
 };
